8e6227bbe68b3e175ad15b12721cc1f8870c0e67
Arthur Gagniare <arthur.gagniare@efrei.net>
2024-11-06 09:38:18 +0100

    v3

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function openProject(url) {
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  function openProject(url) {
     window.open(url, '_blank');
   }
   
